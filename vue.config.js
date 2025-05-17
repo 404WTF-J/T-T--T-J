5765a032b61ce96a4caa77aaad8d469b87e05743
wangtj <wangtj@MacBook-Pro.local>
2025-05-17 18:00:00 +0800

    changePubilcPath

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
-  publicPath: "/T-T---T-J/",
+  publicPath: "/T-T--T-J/",
 });
